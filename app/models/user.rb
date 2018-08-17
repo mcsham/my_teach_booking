6f41272bd5311e389fb93cfda0b4539e03c52580
Daneil Skrybajlo <skrybajlo.d.yu@gmail.com>
2018-08-17 17:23:36 +0300

    fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,14 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
   has_many :authored_reviews, class_name: 'Review'
   has_many :reviews, as: :comment
   belongs_to :city
 
-  validates :name, presence: true
-  validates :surname, presence: true
-  validates :age, presence: true
-
+  validates :name, :surname, :age, presence: true
 end
