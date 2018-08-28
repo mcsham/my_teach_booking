f59df1d395d4a658e8d7865c6fbb990c3d059d72
Daneil Skrybajlo <skrybajlo.d.yu@gmail.com>
2018-08-28 11:42:11 +0300

    add nickname to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_28_081808) do
+ActiveRecord::Schema.define(version: 2018_08_28_083823) do
 
   create_table "categories", force: :cascade do |t|
     t.string "title", null: false
@@ -69,6 +69,7 @@ ActiveRecord::Schema.define(version: 2018_08_28_081808) do
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.integer "role"
+    t.string "nickname", default: "", null: false
     t.index ["city_id"], name: "index_users_on_city_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
